96ec8b8bb01d053345843cf3738d19cba9d310af
shahana <rafiyashahana@gmail.com>
2023-12-13 13:35:06 +0300

    completed the styling

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -4,10 +4,27 @@ const TodoContainer = () => {
     <div>
       <h2>TO DO</h2>
       <div className="container">
-        <input type="text" placeholder="◌  Add a new task..." />
-        <div className="tasks">
-          <span className="task-list">Read for 1 hour</span>
-          <span className="close">×</span>
+        <div className="task-input">
+          <span className="bullet">◌</span>
+          <input type="text" placeholder="Add a new task..." />
+          <span className="add">ADD</span>
+        </div>
+
+        <div className="task-list">
+          <div className="task">
+            <span>Read for 1 hour</span>
+            <span className="close">×</span>
+          </div>
+          <div className="task">
+            <span>Read for 1 hour</span>
+            <span className="close">×</span>
+          </div>
+        </div>
+
+        <div className="task-info">
+          <span>All</span>
+          <span>Active</span>
+          <span>Completed</span>
         </div>
       </div>
     </div>
