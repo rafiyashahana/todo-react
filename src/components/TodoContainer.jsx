c90fd838db79556b6fc025ef2d5c605982942592
shahana <rafiyashahana@gmail.com>
2023-12-13 12:00:39 +0300

    updated the header styles

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -4,7 +4,11 @@ const TodoContainer = () => {
     <div>
       <h2>TO DO</h2>
       <div className="container">
-        <input type="text" placeholder="Add new task" />
+        <input type="text" placeholder="◌  Add a new task..." />
+        <div className="tasks">
+          <span className="task-list">Read for 1 hour</span>
+          <span className="close">×</span>
+        </div>
       </div>
     </div>
   );
