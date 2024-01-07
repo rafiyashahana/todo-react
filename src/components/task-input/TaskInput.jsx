04deb904a4905ab0fac59db4c0448c0543794f38
shahana <rafiyashahana@gmail.com>
2024-01-07 18:27:45 +0300

    added checked class in task-list component

--- src/components/task-input/TaskInput.jsx
+++ src/components/task-input/TaskInput.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./taskInput.styles.css";
 const TaskInput = () => {
+  const [task, setTask] = useState("");
+
+  const handleClick = () => {
+    console.log(task);
+  };
   return (
     <div className="task-input">
       <span className="bullet">◌</span>
-      <input type="text" placeholder="Add a new task..." />
-      <span className="add">ADD</span>
+      <input
+        type="text"
+        placeholder="Add a new task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <span className="add" onClick={handleClick}>
+        ADD
+      </span>
     </div>
   );
 };
--- src/components/task-list/TaskList.jsx
+++ src/components/task-list/TaskList.jsx
@@ -3,6 +3,15 @@ const TaskList = () => {
   return (
     <div className="task-list">
       <div className="task">
+        <span></span>
+        <span>Read for 1 hour</span>
+        <span className="close">×</span>
+      </div>
+      <div className="task checked">
+        <span>Read for 1 hour</span>
+        <span className="close">×</span>
+      </div>
+      <div className="task checked">
         <span>Read for 1 hour</span>
         <span className="close">×</span>
       </div>
