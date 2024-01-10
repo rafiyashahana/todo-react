cb45010c3233fabf69d5827e7d9c21997038ee66
shahana <rafiyashahana@gmail.com>
2024-01-10 08:56:06 +0300

    added active tabs for taskinfo component

--- src/components/task-info/TaskInfo.jsx
+++ src/components/task-info/TaskInfo.jsx
@@ -1,10 +1,24 @@
+import { useContext, useState } from "react";
 import "./taskInfo.styles.css";
+import { TodoContext } from "../../context/todo-context";
 const TaskInfo = () => {
+  const taskInfo = ["All", "Active", "Completed"];
+  // const { isComplete } = useContext(TodoContext);
+  const [active, setActive] = useState(0);
+  const handleClick = (i) => {
+    setActive(i);
+  };
   return (
     <div className="task-info">
-      <span>All</span>
-      <span>Active</span>
-      <span>Completed</span>
+      {taskInfo.map((item, index) => (
+        <span
+          key={index}
+          className={index === active ? "active" : ""}
+          onClick={() => handleClick(index)}
+        >
+          {item}
+        </span>
+      ))}
     </div>
   );
 };
